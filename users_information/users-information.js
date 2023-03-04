d01cb2b9a5d0100a55df8fe8d311c96ec3178943
Beka Sturua <beqasturua3@gmail.com>
2023-03-04 21:15:17 +0400

    feat: added full user information

--- users_information/users-information.js
+++ users_information/users-information.js
@@ -7,11 +7,25 @@ window.addEventListener("load", async () => {
     .then((json) => json);
   let userInfoHTML = "";
   userInfoHTML += `
-    <h2 class="h2">Email:  ${user.email}</h2>
     <h2 class="h2">Name:  ${user.name}</h2>
     <h2 class="h2">Username:  ${user.username}</h2>
+    <h2 class="h2">Email:  ${user.email}</h2>
+    <h2 class="h2">Address</h2>
+    <h2 class="h2">Street:  ${user.address.street}</h2>
+    <h2 class="h2">Street:  ${user.address.suite}</h2>
+    <h2 class="h2">Street:  ${user.address.city}</h2>
+    <h2 class="h2">Street:  ${user.address.zipcode}</h2>
+    <h2 class="h2">Address - Geo</h2>
+    <h2 class="h2">Street:  ${user.address.geo.lat}</h2>
+    <h2 class="h2">Street:  ${user.address.geo.lng}</h2>
     <h2 class="h2">Phone:  ${user.phone}</h2>
     <h2 class="h2">Website:  ${user.website}</h2>
+    <h2 class="h2">Company</h2>
+    <h2 class="h2">Website:  ${user.company.name}</h2>
+    <h2 class="h2">Website:  ${user.company.catchPhrase}</h2>
+    <h2 class="h2">Website:  ${user.company.bs}</h2>
+
+
     `;
   const userInfoListContainer =
     document.getElementsByClassName("user_information")[0];
